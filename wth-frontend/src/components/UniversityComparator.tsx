7b95a97121c6ac2ca3352a4bd5d7bc88239b14a5
eunicesng <eunice.sng.2022@scis.smu.edu.sg>
2023-08-27 16:12:46 +0800

    formatting

--- wth-frontend/src/components/UniversityComparator.tsx
+++ wth-frontend/src/components/UniversityComparator.tsx
@@ -20,7 +20,36 @@ function UniversityComparator (){
         <Button variant="contained" >Compare here!</Button>
       </Stack>
       <Stack spacing={2} sx={{ width: '100%' }} direction="column">
-        <Autocomplete
+      <Box sx={{ width: '100%', bgcolor: 'background.paper' }}  >
+      <Box sx={{ my: 3, mx: 2 }} >
+        <Grid container alignItems="center">
+          <Grid item xs>
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Create a Plan
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography gutterBottom variant="h6" component="div">
+              Total Expenses: $49,950 
+            </Typography>
+          </Grid>
+        </Grid>
+        <Typography color="text.secondary" variant="body2">
+          Enter your information for us to help get your best estimated expenses
+        </Typography>
+      </Box>
+      <Divider variant="middle" />
+      <p></p>
+      <Typography gutterBottom variant="body1" color="blue">
+          Select University & Course
+        </Typography>
+      <Autocomplete
           disableClearable
           options={Universities.map((option) => option.title)}
           renderInput={(params) => (
@@ -48,42 +77,25 @@ function UniversityComparator (){
             />
           )}
         />
-        
-      <Box sx={{ width: '100%', bgcolor: 'background.paper' }}  >
-      <Box sx={{ my: 3, mx: 2 }} >
-        <Grid container alignItems="center">
-          <Grid item xs>
-            <Typography gutterBottom variant="h4" component="div">
-              Your Eligibility
-            </Typography>
-          </Grid>
-          <Grid item>
-            <Typography gutterBottom variant="h6" component="div">
-              Total Expenses: $49,950 
-            </Typography>
-          </Grid>
-        </Grid>
-        <Typography color="text.secondary" variant="body2">
-          Enter your information for us to help get your best estimated expenses
-        </Typography>
-      </Box>
-      <Divider variant="middle" />
       <Box sx={{ m: 2 }}>
-        <Typography gutterBottom variant="body1">
+      <Typography gutterBottom variant="body1" color="blue">
           Select nationality
-        </Typography>
+      </Typography>
+      <p></p>
         <Stack direction="row" spacing={1}>
           <Chip color="primary" label="Singaporean Citizen" />
           <Chip label="Singapore PR" />
           <Chip label="Non-Singaporean" />
         </Stack>
+        <p></p>
         You are eligible for a 4 year tuition grant.
       </Box>
       <Divider variant="middle" />
       <Box sx={{ m: 2 }}>
-        <Typography gutterBottom variant="body1">
+      <Typography gutterBottom variant="body1" color="blue">
           Select Gross Household Income/Per Capita Income
         </Typography>
+        <p></p>
         <Stack direction="row" spacing={1}>
           <Chip color="primary" label="GHI ≤$3,000 or PCI ≤ $750" />
           <Chip label="GHI $3,001-$4,400 or PCI $751-$1,100" />
@@ -91,13 +103,15 @@ function UniversityComparator (){
           <Chip label="GHI $7,501-$10,000 or PCI $1,876-$2,500" />
           <Chip label="Others" />
         </Stack>
+        <p></p>
         You are eligible for Higher Education Community Bursary: $6,300 per annum.
       </Box>
       <Divider variant="middle" />
       <Box sx={{ m: 2 }}>
-        <Typography gutterBottom variant="body1">
+      <Typography gutterBottom variant="body1" color="blue">
           Graduating in
         </Typography>
+        <p></p>
         <Slider
           aria-label="Temperature"
           defaultValue={4}
@@ -112,7 +126,7 @@ function UniversityComparator (){
 
       <Divider variant="middle" />
       <Box sx={{ m: 2 }}>
-        <Typography gutterBottom variant="body1">
+      <Typography gutterBottom variant="body1" color="blue">
           Personalise the expense tracker with other categories
         </Typography>
         <Button variant="contained">
